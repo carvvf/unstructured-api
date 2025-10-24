8ac59f0ded84e9fc8a01cb038f9777718dd8bb58
carvvf <carvvf@gmail.com>
2025-10-24 17:24:13 +0200

    added sample-docs to curltest.sh

--- scripts/curltest.sh
+++ scripts/curltest.sh
@@ -6,14 +6,17 @@ set -euo pipefail
 LAYOUT_MODEL="yolox"
 
 #FILE_PATH="/home/carlo/AI/"
-FILE_PATH="/home/carlo/Documenti/AI/data/odg_2023/"
+#FILE_PATH="/home/carlo/Documenti/AI/data/odg_2023/"
 
-FILE_NAME="odg005_11_01_23.pdf"
+#FILE_NAME="odg005_11_01_23.pdf"
 #FILE_NAME="odg019_16_02_23.pdf"
 #FILE_NAME="odg031_21_03_23.pdf"
 #FILE_NAME="odg041_10_05_23.pdf"
 #FILE_NAME="odg052_15_06_23_all_2.pdf"
 #FILE_NAME="odg064_08_09_23_All_4.pdf"
+
+FILE_PATH="/home/carlo/Documenti/AI/source/ingestion/unstructured-api/sample-docs/"
+FILE_NAME="layout-parser-paper.pdf"
 
 curl -X POST http://localhost:8000/general/v0/general \
   -H 'unstructured-api-key: sk-fake-api-key' \
@@ -34,3 +37,4 @@ curl -X POST http://localhost:8000/general/v0/general \
 
 source ../../.uapi_venv_cpu/bin/activate
 ./json2pdf.py $FILE_PATH$FILE_NAME.json --hover-tooltips
+deactivate
