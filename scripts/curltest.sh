8c7885ba062b9fdf463e5613330d918a767880de
carvvf <carvvf@gmail.com>
2025-10-27 19:11:28 +0100

    added comments and test files to curltest.sh

--- scripts/curltest.sh
+++ scripts/curltest.sh
@@ -8,16 +8,16 @@ LAYOUT_MODEL="yolox"
 #FILE_PATH="/home/carlo/AI/"
 FILE_PATH="/home/carlo/Documenti/AI/data/odg_2023/"
 
-#FILE_NAME="odg005_11_01_23.pdf"
-#FILE_NAME="odg019_16_02_23.pdf"
-#FILE_NAME="odg031_21_03_23.pdf"
-#FILE_NAME="odg041_10_05_23.pdf"
-#FILE_NAME="odg052_15_06_23_all_2.pdf"
-#FILE_NAME="odg064_08_09_23_All_4.pdf"
-#FILE_NAME="odg063_05_09_23.pdf"
-#FILE_NAME="odg057_09_08_23.pdf"
-#FILE_NAME="odg063_05_09_23.pdf"
-FILE_NAME="odg085_23_10_23.pdf"
+#FILE_NAME="odg005_11_01_23.pdf"        # Modello valutazione annuale
+#FILE_NAME="odg019_16_02_23.pdf"        # Tabella a pag. 3
+#FILE_NAME="odg031_21_03_23.pdf"        # Numerose tabelle
+#FILE_NAME="odg041_10_05_23.pdf"        # Testo scansionato
+#FILE_NAME="odg052_15_06_23_all_2.pdf"  # Brochure con tabella
+#FILE_NAME="odg057_09_08_23.pdf"        # Testo verticale a pag. 3 (all'interno di un box del layout)
+#FILE_NAME="odg063_05_09_23.pdf"        # Testo verticale a pag. 4
+#FILE_NAME="odg064_08_09_23_All_4.pdf"  # Testo misto nativo + scansione
+#FILE_NAME="odg085_23_10_23.pdf"        # Testo misto nativo + scansione
+FILE_NAME="odg106_06_11_23.pdf"         # Tutte le pagine al contrario + alcune tabelle ruotate
 
 #FILE_PATH="/home/carlo/Documenti/AI/source/ingestion/unstructured-api/sample-docs/"
 #FILE_NAME="layout-parser-paper.pdf"
